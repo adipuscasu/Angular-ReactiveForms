ffdb9d52f7c02a7bd28cc1c4ef0d7d6c8a88fe6f
Adrian Puscasu <apu@f5it.ro>
2019-07-31 00:17:35 +0300

    adds custom valdator

--- Demo-Start/src/app/customers/customer.component.ts
+++ Demo-Start/src/app/customers/customer.component.ts
@@ -1,7 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Customer } from './customer';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, AbstractControl, ValidatorFn } from '@angular/forms';
+
+function ratingRange(min: number, max: number): ValidatorFn {
+  return (c: AbstractControl): { [key: string]: boolean } | null => {
+    if (c.value !== null && (isNaN(c.value) || c.value < min || c.value > max)) {
+      return { 'range': true };
+    }
+    return null;
+  }
+}
 
 @Component({
   selector: 'app-customer',
@@ -12,19 +21,41 @@ export class CustomerComponent implements OnInit {
   customer = new Customer();
   public customerForm: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
-    this.customerForm = new FormGroup({
-      firstName: new FormControl(),
-      lastName: new FormControl(),
-      email: new FormControl(),
-      sendCatalog: new FormControl(true)
+    this.customerForm = this.formBuilder.group({
+      firstName: ['', [Validators.required, Validators.minLength(3)]],
+      lastName: ['', [Validators.required, Validators.minLength(50)]],
+      email: ['', [Validators.required, Validators.email]],
+      phone: '',
+      notification: 'email',
+      sendCatalog: true,
+      rating: [null, ratingRange(1,5)]
     });
   }
 
   save() {
     console.log(this.customerForm);
     console.log('Saved: ' + JSON.stringify(this.customerForm.value));
+  }
+
+  setNotification(notifyVia: string): void {
+    const emailControl = this.customerForm.get('email');
+    const phoneControl = this.customerForm.get('phone');
+    phoneControl.clearValidators();
+    emailControl.clearValidators();
+
+
+    if (notifyVia === 'email') {
+      emailControl.setValidators([Validators.required, Validators.email]);
+
+    } else {
+      phoneControl.setValidators(Validators.required);
+    }
+    phoneControl.updateValueAndValidity();
+    emailControl.updateValueAndValidity();
+
+
   }
 }
